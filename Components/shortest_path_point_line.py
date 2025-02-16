41ad34740a9b9274c5ae40e34a82367238864bb3
Darakhsh1999 <darakrash@protonmail.com>
2025-02-16 18:42:03 +0100

    shortest path algorithm done

--- Components/shortest_path_point_line.py
+++ Components/shortest_path_point_line.py
@@ -1,8 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = 10
+
+p1 = np.array([1,1])
+p2 = np.array([5,3])
+p3 = np.array([0,8])
+P = np.vstack((p1,p2,p3))
+print(P.shape)
+
+# draw line between P1 P2: d
+d = (p2 - p1)
+
+# calculate vector P1->P3
+p3p1 = p3 - p1
 
 
-points = L*np.random.random((3,2))
-print(points)
+# shortest distance vector is orthogonal projection of P1->P3 on line d
+projection = (np.dot(p3p1,d) / (np.dot(d,d))) * d
+orthogonal = p3p1 - projection
+intersection = p3 - orthogonal
+
+
+
+# DONT CHANGE
+plt.scatter(P[:,0],P[:,1]) # all points
+plt.plot([p1[0],p2[0]], [p1[1],p2[1]], "-") # lines between p1 p2
+
+plt.plot([p3[0],intersection[0]], [p3[1],intersection[1]], "-") # lines between p3 intersection
+
+
+plt.grid()
+plt.xlim([0,10])
+plt.ylim([0,10])
+plt.xticks(np.arange(10))
+plt.yticks(np.arange(10))
+plt.show()
